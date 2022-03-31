b305eedd91fd92be55ec02ab76b7b7fcd0c44d75
Linda Hurtig <lindahurtig93@gmail.com>
2022-03-31 15:56:27 +0200

    Staff och Store, entity, getters,setters..

--- src/main/java/se/ya/videobutik/model/Staff.java
+++ src/main/java/se/ya/videobutik/model/Staff.java
@@ -1,4 +1,137 @@
 package se.ya.videobutik.model;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
 public class Staff {
+
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "staff_id")
+    private int staff_id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ManyToOne
+    @JoinColumn(name = "address_id", referencedColumnName = "address_id")
+    private Address address;
+
+   /* @Column(picture = "picture")
+    private blob picture; */
+
+    @Column(name = "email")
+    private String email;
+
+    @ManyToMany
+    private Store store;
+
+    @Column(name = "active")
+    private boolean active;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "last_update")
+    private Date lastUpdate;
+
+    public Staff() {
+    }
+
+    public Staff(String first_name, String lastName, Address address, String email, Store store, boolean active) {
+        this.first_name = first_name;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.store = store;
+        this.active = active;
+    }
+
+    public int getStaff_id() {
+        return staff_id;
+    }
+
+    public void setStaff_id(int staff_id) {
+        this.staff_id = staff_id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
--- src/main/java/se/ya/videobutik/model/Store.java
+++ src/main/java/se/ya/videobutik/model/Store.java
@@ -1,5 +1,55 @@
 package se.ya.videobutik.model;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
 public class Store {
 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "store_id")
+    private int store_id;
+
+    // @Column(name = "manager_staff_id")
+
+    @OneToOne
+    @JoinColumn(name = "address_id", referencedColumnName = "address_id")
+    private Address address;
+
+    @Column(name = "last_update")
+    private Date lastUpdate;
+
+    public Store() {
+    }
+
+    public Store(int store_id, Address address, Date lastUpdate) {
+        this.store_id = store_id;
+        this.address = address;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public int getStore_id() {
+        return store_id;
+    }
+
+    public void setStore_id(int store_id) {
+        this.store_id = store_id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Date getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Date lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
 }
