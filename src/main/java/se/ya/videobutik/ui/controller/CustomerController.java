4e3bd4ee2f5b9a03ace1eba63a3ddd5544f8dfff
Linda Hurtig <lindahurtig93@gmail.com>
2022-04-03 20:08:26 +0200

    lagt in fx:idn och fixat gui till staff sidan

--- src/main/java/se/ya/videobutik/ui/controller/CustomerController.java
+++ src/main/java/se/ya/videobutik/ui/controller/CustomerController.java
@@ -1,6 +1,41 @@
 package se.ya.videobutik.ui.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class CustomerController {
+
+    @FXML private Button btn_add_costumer;
+    @FXML private Button btn_delete_costumer;
+    @FXML private Button btn_edit_costumer;
+
+    @FXML private ComboBox<?> cb_add_city;
+    @FXML private ComboBox<?> cb_edit_city;
+
+    @FXML private CheckBox checkb_add_active;
+    @FXML private CheckBox checkb_edit_active;
+
+    @FXML private TextField tf_add_address;
+    @FXML private TextField tf_add_district;
+    @FXML private TextField tf_add_email;
+    @FXML private TextField tf_add_first_name;
+    @FXML private TextField tf_add_last_name;
+    @FXML private TextField tf_add_phone;
+    @FXML private TextField tf_add_postal_code;
+    @FXML private TextField tf_edit_address;
+    @FXML private TextField tf_edit_district;
+    @FXML private TextField tf_edit_email;
+    @FXML private TextField tf_edit_first_name;
+    @FXML private TextField tf_edit_last_name;
+    @FXML private TextField tf_edit_phone;
+    @FXML private TextField tf_edit_postal_code;
+    @FXML private TextField tf_find_last_name;
+
+    @FXML private TableView<?> tv_customers;
 
 }
 
--- src/main/java/se/ya/videobutik/ui/controller/MovieController.java
+++ src/main/java/se/ya/videobutik/ui/controller/MovieController.java
@@ -2,18 +2,30 @@ package se.ya.videobutik.ui.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 public class MovieController {
+    @FXML private Button btn_add;
+    @FXML private Button btn_delete;
+    @FXML private Button btn_edit;
+    @FXML private Button btn_select;
 
-    @FXML
-    private Button btn_search;
+    @FXML private ComboBox<?> cb_add_language;
+    @FXML private ComboBox<?> cb_edit_language;
 
-    @FXML
-    private ListView<?> lv_movies;
+    @FXML private ListView<?> lv_films;
 
-    @FXML
-    private TextField tf_title;
+    @FXML private TextField tf_add_rental_duration;
+    @FXML private TextField tf_add_rental_rate;
+    @FXML private TextField tf_add_replacement_cost;
+    @FXML private TextField tf_add_title;
+    @FXML private TextField tf_edit_rental_duration;
+    @FXML private TextField tf_edit_rental_rate;
+    @FXML private TextField tf_edit_title;
+    @FXML private TextField tf_film_id;
+    @FXML private TextField tf_replacement_cost;
+
 
 }
