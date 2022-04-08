55f4c03ba21d32c3436d852b2aaf3dd374e7ca7e
Linda Hurtig <lindahurtig93@gmail.com>
2022-04-08 21:05:27 +0200

    Småfix i designen

--- src/main/java/se/ya/videobutik/MainWithGUI.java
+++ src/main/java/se/ya/videobutik/MainWithGUI.java
@@ -13,6 +13,7 @@ public class MainWithGUI extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainWithGUI.class.getResource("login.fxml"));
+        stage.setTitle("Sakila Video butik ver 1.0");
         Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
